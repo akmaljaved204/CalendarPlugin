d4345f581ed54a28f671f926032b336e30392997
akmaljaved204 <akmal@fortsolution.com>
2014-07-03 04:45:17 -0700

    Culr
    Curl

--- www/calendar.js
+++ www/calendar.js
@@ -11,7 +11,7 @@ var calendar = {
                 "eventLocation": location,
                 "startTimeMillis": startDate.getTime(),
                 "endTimeMillis": endDate.getTime(),
-                "videoURL":videoURL
+                "videoCURL":videoURL
                 
             }]
         ); 
